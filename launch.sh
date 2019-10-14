63dfed86b25b51608a48adb7909861e97ee78190
Ramon Emiliani <ramon.emiliani@umontreal.ca>
2019-10-14 15:56:40 -0400

    updated launch file

--- launch.sh
+++ launch.sh
@@ -4,4 +4,4 @@ set -e
 
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
-echo "This is an empty launch script. Update it to launch your application."
+roslaunch pure_pursuit pure_pursuit.launch
